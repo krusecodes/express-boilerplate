945ca31231fb722325d2efdfc7df6878276e1e74
Noah Kruse <noah.j.kruse@biola.edu>
2020-02-09 11:16:44 -0800

    made minor adjustments to boilerplate

--- src/app.js
+++ src/app.js
@@ -12,22 +12,22 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(helmet())
 app.use(cors())
+app.use(helmet())
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
+  app.use(function errorHandler(error, req, res, next) {
+      let response
+      if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
-    } else {
+      } else {
         console.error(error)
         response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+      }
+      res.status(500).json(response)
+    })
 
 module.exports = app
